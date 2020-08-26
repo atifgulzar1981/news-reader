e45bc7e70526326eb4fe401073ba2e55744b7713
atifgulzar <aatiff9@hotmail.com>
2020-08-27 00:08:52 +0400

    Design changes.

--- src/components/StoriesContainer.tsx
+++ src/components/StoriesContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { HackerNewsState } from "../types";
@@ -12,21 +12,30 @@ export default () => {
   );
 
   useEffect(() => {
+    // I know redux for this thing is over kill
+    // and this could be done using local state
+    // I am using redux just for demonstration purpose.
     dispatch(getStoryIds());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [dispatch]);
 
-  const currentPageStoryIds: number[] = [];
-  if (appState.storyIds && appState.storyIds.length > 0) {
-    for (
-      let i = appState.currentPageStartIndex;
-      i < appState.currentPageLastIndex;
-      i++
-    ) {
-      currentPageStoryIds.push(appState.storyIds[i]);
+  const currentPageStoryIds: number[] = useMemo(() => {
+    const data = [];
+
+    if (appState.storyIds && appState.storyIds.length > 0) {
+      for (
+        let i = appState.currentPageStartIndex;
+        i < appState.currentPageLastIndex;
+        i++
+      ) {
+        data.push(appState.storyIds[i]);
+      }
     }
-  }
-  console.log(appState);
+    return data;
+  }, [
+    appState.currentPageLastIndex,
+    appState.currentPageStartIndex,
+    appState.storyIds,
+  ]);
 
   const gotoNextPage = () => {
     dispatch(getNewPage());
@@ -37,23 +46,27 @@ export default () => {
   }
 
   return (
-    <div className="container">
-      <p> </p>
-      <h6 className="text-white bg-dark">New Stories</h6>
-      <p> </p>
-      {currentPageStoryIds.map((storyId, index) => {
-        return (
-          <Story
-            key={storyId}
-            storyId={storyId}
-            index={++index}
-            pageStartIndex={appState.currentPageStartIndex}
-          />
-        );
-      })}
-      <button className="btn btn-xs btn-primary" onClick={() => gotoNextPage()}>
-        More
-      </button>
+    <div className="container" data-testid="stories-container">
+      <h6 className="text-black story-title mt-3 mb-3 p-2">New Stories</h6>
+      <div className="story-container">
+        {currentPageStoryIds.map((storyId, index) => {
+          return (
+            <Story
+              key={index}
+              storyId={storyId}
+              index={++index}
+              pageStartIndex={appState.currentPageStartIndex}
+            />
+          );
+        })}
+
+        <button
+          className="btn btn-sm btn-secondary mt-3"
+          onClick={() => gotoNextPage()}
+        >
+          More
+        </button>
+      </div>
       <p>&nbsp;</p>
     </div>
   );
